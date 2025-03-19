7fb5f8358375db7824f3289d8c1753eaffc83eaf
whernandez94 <grupogd.whernandez@gmail.com>
2025-03-19 07:27:36 -0600

    Example with signal computed and import pipe uppercase

--- src/app/pages/hero/hero-page.component.ts
+++ src/app/pages/hero/hero-page.component.ts
@@ -1,16 +1,23 @@
-import { Component, signal } from "@angular/core";
+import { UpperCasePipe } from "@angular/common";
+import { Component, computed, signal } from "@angular/core";
 
 @Component({
-  templateUrl: './hero-page.component.html'
+  templateUrl: './hero-page.component.html',
+  imports: [
+    UpperCasePipe
+  ]
 })
 export class HeroPageComponent {
 
   name = signal('Ironman');
   age = signal(45);
 
-  getHeroDescription() {
-    return `${ this.name() } - ${ this.age() }`;
-  }
+  heroDescription = computed(() => {
+    const description = `${ this.name() } - ${ this.age() }`;
+    return description;
+  });
+
+  capitalizeName = computed(() => this.name().toUpperCase());
 
   changeHero() {
     this.name.set('Spiderman');
